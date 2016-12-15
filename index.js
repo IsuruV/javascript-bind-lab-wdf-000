3e7cdbea2bfa5c8e5f92eb1d7f0a47228d85beaf
Isuru <github email address>
2016-12-15 12:09:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much.";
+
+var digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking(){
+    return setInterval(() => this.time++, 1000);
+  }
+}
+
+var censor = (word,str) => {
+  return str.replace(new RegExp(word, 'g'), "BLEEP");
+}
+
+var drugsCensor = (str) => {
+  return str.replace(new RegExp('drugs', 'g'), "BLEEP");
+}
+
+var violenceCensor = (str) => {
+  return str.replace(new RegExp('violence', 'g'), "BLEEP");
+}
